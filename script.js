dcfc0e81dbf054f2fd8776a35f3ca4fea20c7347
Steven Stefanov <77702697+stevenstefanov@users.noreply.github.com>
2021-02-18 18:21:50 -0500

    added if a NaN is imput. created for loop for passwordMix.

--- script.js
+++ script.js
@@ -1,42 +1,77 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var characters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", "?", "~" ];
 
+var lowerLetters = ["abcdefghijklmnopqrstuvwxyz"];
+var upperLetters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var numbers = ["0123456789"];
+var characters = ["!@#$%^&*()-+?~"];
 
 
 // Write password to the #password input
 
-var inputOptions = function() {
+var generatePassword = function() {
 
   // Ask user how many characters they would like their password to contain.
-
+  var passwordMix = [];
+  var criteriaConfirm
   var lengthSelection = window.prompt("Please enter a number that has between 8 - 128 characters:");      
   if (!lengthSelection) {
-    return;
+    return("");
   }
   else if (lengthSelection < 8 || lengthSelection > 128) {
-    window.prompt("Please enter a number that has between 8 - 128 characters.");
-    inputOptions();
+    criteriaConfirm = window.confirm("Your selection does not meet the criteria provided. Please try again.");
+    if (!criteriaConfirm) {
+      return("");
+    }
+    if (criteriaConfirm) {
+    generatePassword();
+    }
   }
   else if (isNaN(lengthSelection)) {
-    window.prompt("Please enter a number that has between 8 - 128 characters.");
-    inputOptions();
-  }
-  else {
-    var passLength = userInput;
-    return;
+    criteriaConfirm = window.confirm("Your selection does not meet the criteria provided. Please try again.");
+    if (!criteriaConfirm) {
+      return("");
+    }
+    if (criteriaConfirm) {
+    generatePassword();
+    }
+    else {
+      return;
+    }
   }
 
-  var lowerSelection = confirm("Please indicate if you would like your password to contain lowercase letters:");
-  var upperSelection = confirm("Please indicate if you would like your password to contain uppercase letters:");
-  var numberSelection = confirm("Please indicate if you would like your password to contain numbers:");
-  var specialSelection = confirm("Please indicate if you would like your password to contain special characters:");
-
+  var passwordSelection = [];
   
-}
+  var lowerSelection = confirm("Please indicate if you would like your password to contain lowercase letters (ex. a, b, c):");
+  
+  if (lowerSelection) {
+    passwordSelection += lowerLetters;
+  }
+  
+  var upperSelection = confirm("Please indicate if you would like your password to contain uppercase letters (ex. A, B, C):");
+  
+  if (upperSelection) {
+    passwordSelection += upperLetters;
+  }
+
+  var numberSelection = confirm("Please indicate if you would like your password to contain numbers (ex. 0, 1, 2):");
+ 
+  if (numberSelection) {
+    passwordSelection += numbers;
+  }
+
+  var characterSelection = confirm("Please indicate if you would like your password to contain special characters (ex. $, #, %):");
+
+  if (characterSelection) {
+    passwordSelection += characters;
+  }
+  
+
+  for (var i = 0; i < lengthSelection; i++) {
+    passwordMix = passwordMix + passwordSelection[Math.floor(Math.random() * passwordSelection.length)];
+  }
+  return passwordMix;
+};
 
 function writePassword() {
   var password = generatePassword();
@@ -48,12 +83,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-var options = function() {
-  var lowerCase = window.confirm("Would you like your password to contain lowercase letters? (ex. a, b, c)");
-  var upperCase = window.confirm("Would you like your password to contain uppercase letters? (ex. A, B, C)");
-}
-
-inputOptions();
