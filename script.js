220b5bb4e0e74cbb5a60a76323466c1fab5f9851
Steven Stefanov <77702697+stevenstefanov@users.noreply.github.com>
2021-02-18 14:34:18 -0500

    switched password generating function to inputOptions. Added vars lengthSelection, lowerSelectionl upperSelection, numberSelection, specialSelection.

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var characters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", "?", "~" ];
 
 
 
 // Write password to the #password input
+
+var inputOptions = function() {
+
+  // Ask user how many characters they would like their password to contain.
+
+  var lengthSelection = window.prompt("Please enter a number that has between 8 - 128 characters:");      
+  if (!lengthSelection) {
+    return;
+  }
+  else if (lengthSelection < 8 || lengthSelection > 128) {
+    window.prompt("Please enter a number that has between 8 - 128 characters.");
+    inputOptions();
+  }
+  else if (isNaN(lengthSelection)) {
+    window.prompt("Please enter a number that has between 8 - 128 characters.");
+    inputOptions();
+  }
+  else {
+    var passLength = userInput;
+    return;
+  }
+
+  var lowerSelection = confirm("Please indicate if you would like your password to contain lowercase letters:");
+  var upperSelection = confirm("Please indicate if you would like your password to contain uppercase letters:");
+  var numberSelection = confirm("Please indicate if you would like your password to contain numbers:");
+  var specialSelection = confirm("Please indicate if you would like your password to contain special characters:");
+
+  
+}
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -18,18 +49,11 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var lengthSelector = function() {
-  var userInput = window.prompt("Please enter a number that has between 8 - 128 characters.");
-  if (!userInput) {
-    return;
-  }
-  else if (userInput < 8 || userInput >128) {
-    window.alert("Please enter a number that has between 8 - 128 characters.");
-    return;
-  }
-  else {
-    var passLength = userInput;
-    console.log(passLength);
-    return;
-  }
-};
+
+
+var options = function() {
+  var lowerCase = window.confirm("Would you like your password to contain lowercase letters? (ex. a, b, c)");
+  var upperCase = window.confirm("Would you like your password to contain uppercase letters? (ex. A, B, C)");
+}
+
+inputOptions();
